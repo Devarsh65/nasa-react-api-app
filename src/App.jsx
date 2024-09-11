2c7b8ccf1bf18062b09cf38b5eb72e6e0c4dfdd3
Devarsh <devarshshah9234@gmail.com>
2024-09-11 18:53:09 +0530

    project data

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./component/Footer"
+import Sidebar from "./component/Sidebar"
+import Main from "./component/Main"
+
+function App() {
+
+  return (
+    <>
+    <Sidebar>ASDASD</Sidebar>
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
